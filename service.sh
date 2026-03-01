9997e54547a53bda9e7fa3f5b518f19199f660b1
zx1101 <78824368+zx1101@users.noreply.github.com>
2026-03-01 23:03:43 +0300

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,8 +1,5 @@
 resetprop --delete ro.lineage.build.version
-resetprop --delete ro.lineage.build.version.plat.rev
-resetprop --delete ro.lineage.build.version.plat.sdk
 resetprop --delete ro.lineage.device
 resetprop --delete ro.lineage.display.version
 resetprop --delete ro.lineage.releasetype
 resetprop --delete ro.lineage.version
-resetprop --delete ro.lineagelegal.url
